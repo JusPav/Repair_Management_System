35993a3d9c5e6dc91a7c04debaebfdf342c9ae36
JusPav <justinas.pavasaris@gmail.com>
2023-06-18 22:30:26 +0300

    Adding main.

---
+++ src/main/java/lt/code/academy/repair_management_system/RepairManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lt.code.academy.repair_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/code/academy/repair_management_system/RepairManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.code.academy.repair_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
